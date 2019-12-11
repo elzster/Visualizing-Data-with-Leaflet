207bcd961ef7411fd61c00ab35c876a4c8ce0b58
elzster <hakim.elie1@gmail.com>
2019-12-11 17:42:34 -0500

    update to files

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZWxpZXZhdG9yIiwiYSI6ImNrM3MyN2kxdDAybDEzbnFta2t0cWZhNXgifQ.n673Ox5Be2IEie3hpl4Ozw";
---
+++ static/js/logic.js
@@ -0,0 +1,90 @@
+var earthquakeurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+ 
+var sanFranCoords = [37.77885586164994, -122.4213409423828]
+
+d3.json(earthquakeurl, data => {
+    createFeatures(data.features);
+});
+
+function getColor(d) {
+
+    return d < 1 ? 'green' : 
+           d < 2 ? 'green' :
+           d < 3 ? 'lightgreen' :
+           d < 4 ? 'orange' :
+           d < 5 ? 'orange':
+                   'red';
+}
+
+function createFeatures(earthquakeData) {
+
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3 align='center'>" + feature.properties.place +
+            "</h3><hr><p><u>Occurrence:</u> " + new Date(feature.properties.time) + "</p>" +
+            "</h3><p><u>Magnitude:</u> " + feature.properties.mag + "</p>");
+    }
+
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer:  (feature, latlng) => {
+            var geojsonMarkerOptions = {
+            radius: 4*feature.properties.mag,
+            fillColor: getColor(feature.properties.mag),
+            color: "black",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.9
+            };
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    });
+    
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: API_KEY
+    });
+ 
+    var baseMaps = {
+        "Light Map": lightmap 
+    };
+    
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+
+    var map = L.map("map", {
+        center: sanFranCoords,
+        zoom: 4.5,
+        layers: [lightmap, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {collapsed: false})
+             .addTo(map);
+
+    var legend = L.control({position: 'bottomright'});
+  
+    legend.onAdd = map => {    
+        var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+  
+        div.innerHTML+='Magnitude<br><hr>'
+    
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    
+    return div;
+    };
+    
+    legend.addTo(map);
+}
